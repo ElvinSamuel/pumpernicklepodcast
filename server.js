450bba1a1e5342b9a903d8b152feafcccdb40cfe
Elvin Samuel <elvinsamuel1@gmail.com>
2018-04-27 19:30:46 +0000

    Additional server.js info

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res){
     res.sendFile(index);
 });
 
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log(`The Bread Is Rising On Port ${process.env.PORT}`);
 });
