261f86b6c26d8e588086a448b41aa8dc075ec1b7
Elvin Samuel <elvinsamuel1@gmail.com>
2018-04-29 22:01:59 +0000

    Correction on the sendFile page

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 
+
 app.get('/', function(req, res){
     res.sendFile(index);
 });
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
