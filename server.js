2fd08e110e6facbea6958f3c829981ac7cafb934
Elvin Samuel <elvinsamuel1@gmail.com>
2018-08-28 02:25:45 +0000

    Simplified the website design, removing unnecessary folders

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express'),
-	index   = require('./routes/index.js'),
 	path    = require('path'),
 	
 	app     = express();
 
 // Routes =============
+
 app.use(express.static(path.join(__dirname + '/public')));
-
-
-// ====================
-app.use('/', index);
 // ====================
 
+app.get('*', function(req, res) {
+		console.log('Router is working!');
+		res.sendFile('index.html');
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log(`The Bread Is Rising On Port ${process.env.PORT}`); 
